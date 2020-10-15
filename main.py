eb63985842b524fc23828f4de6e92f807c76266f
Rain <32306147+RainKins@users.noreply.github.com>
2020-10-15 12:19:32 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# Needed to randomize choices
+import random
+
+
+# Define the restart program option
+def restart():
+    restart_choice = input("\nRestart (Y/N): ")
+    restart_choice = restart_choice.upper()
+
+    if restart_choice == "Y":
+        print("\n" * 10)
+        main()
+    elif restart_choice == "N":
+        print("\nOkay. Good luck with your decision!")
+    else:
+        print("Invalid key. Try again")
+        restart()
+
+
+# The program must be ran in main
+def main():
+    # declare choices as a list
+    choices = []
+
+    # Ask user for the amount of choices they have for their decision
+    option_count = input("How many options are you debating? ")
+    print("\n")
+
+    # Check for any keys that can't be changed into an integer
+    if option_count.isnumeric():
+
+        # Take the user's option_count input to create a for loop to add their options to the choices list
+        for i in range(int(option_count)):
+            option = input('Option: ')
+            choices.append(option)
+
+        # Shuffle choices list in order to get a randomized result multiple times, rather than the same random choice from the first run
+        for i in range(len(choices) + 1):
+            random.shuffle(choices)
+
+        decision = choices[0]
+
+        # Display user's decision
+        print('\nYour randomized decision is ' + '"' + decision + '"' + '.')
+
+        # Ask user if they want to  restart the program
+        restart()
+
+    # If key can't be changed into an integer, restart program
+    else:
+        print("Invalid Key. Try again.")
+        print("\n" * 9)
+        main()
+
+
+main()
